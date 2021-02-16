8a6a4428eb0d6ad58430d1fbeaa2e658af13e98e
suShinGG <chin.lnw@gmail.com>
2021-02-16 22:50:20 +0700

    Add bot color support
    group name
    - Crypto Up
    - Crypyo Down
    - CryptoCurrency

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ let guildMeCache = [];
 let ticker_list = [];
 let priceData = [];
 let TimeOut = [];
+let botArrow = []
 for (let i = 1; i <= bot_count; i++) {
   let bot_num = i - 1;
   let TICKER = process.env[`SYMBOL_${bot_num}`]
@@ -40,14 +41,15 @@ for (let i = 1; i <= bot_count; i++) {
   bot.on('rateLimit', (e) => {
     if (typeof e.timeout !== undefined && e.path.includes('@me/nick')) {
       TimeOut[bot_num] = Date.now() + e.timeout + 3000
-      console.log(`${Date.now()}-${TICKER}:time out ${e.timeout}`,TimeOut[bot_num])
+      console.log(`${Date.now()}-${TICKER}:time out ${e.timeout}`, TimeOut[bot_num])
+
     }
   })
 
   bot.on('message', (msg) => {
     let { guildID, channelID, authorID, content } = msg
-    console.log(msg)
-    console.log(guildID, channelID, authorID, content)
+    // console.log(msg)
+    // console.log(guildID, channelID, authorID, content)
     // filters only for commands that start with '!'
     if (content.substring(0, 1) == '!') {
       var args = content.substring(1).split(' ');
@@ -55,6 +57,18 @@ for (let i = 1; i <= bot_count; i++) {
       var param = args[1]
       if (command === `${TICKER.toLowerCase()}ping`) {
         msg.channel.send('pong');
+      }
+      if (command === `${TICKER.toLowerCase()}test`) {
+        msg.channel.send('pong');
+        console.log('-------------------------------')
+        guildMeCache[bot_num].forEach(guildMe => {
+          let role_up = guildMe.guild.roles.cache.find(e => e.name == 'Crypto Up')
+          let role_down = guildMe.guild.roles.cache.find(e => e.name == 'Crypto Down')
+          let role_base = guildMe.guild.roles.cache.find(e => e.name == 'CryptoCurrency')
+          guildMe.roles.add(role_down)
+          guildMe.roles.remove(role_up)
+          guildMe.roles.remove(role_base)
+        });
       }
     }
   });
@@ -71,15 +85,43 @@ socketClient.setHandler('24hrTicker', (params) => {
     }
   }
 });
-
+function setGroup(guildMe, status) {
+  let role_up = guildMe.guild.roles.cache.find(e => e.name == 'Crypto Up')
+  let role_down = guildMe.guild.roles.cache.find(e => e.name == 'Crypto Down')
+  let role_base = guildMe.guild.roles.cache.find(e => e.name == 'CryptoCurrency')
+  if (status == 'up') {
+    guildMe.roles.add(role_up)
+    guildMe.roles.remove(role_down)
+    guildMe.roles.remove(role_base)
+  }
+  if (status == 'down') {
+    guildMe.roles.add(role_down)
+    guildMe.roles.remove(role_up)
+    guildMe.roles.remove(role_base)
+  }
+  if (status == 'equal') {
+    guildMe.roles.add(role_base)
+    guildMe.roles.remove(role_down)
+    guildMe.roles.remove(role_up)
+  }
+}
 function showPrice(bot_num) {
   if (priceData?.[bot_num] == undefined) return;
   let change24H = priceData[bot_num].P
   let lastPRICE = priceData[bot_num].c
   let changeArrow = change24H > 0 ? '(↗)' : (change24H < 0 ? '(↘)' : '(→)')
+  if (changeArrow != botArrow[bot_num]) {
+    let group = change24H > 0 ? 'up' : (change24H < 0 ? 'down' : 'equal')
+    guildMeCache[bot_num].forEach(guildMe => {
+      setGroup(guildMe, group)
+    });
+    botArrow[bot_num] = changeArrow
+  }
+  botArrow[bot_num] = changeArrow
   let coin = ticker_list[bot_num].toUpperCase()
   if (Date.now() < TimeOut[bot_num]) return;
   guildMeCache[bot_num].forEach(guildMe => guildMe.setNickname(`${coin} :24h ${parseFloat(change24H).toFixed(2)}%`));
   bot_group[bot_num].user.setActivity(`$ ${lastPRICE} ${changeArrow}`);
+
   //console.log(`${coin.toUpperCase()} 24hr change ${change24H} ${changeArrow} $ ${lastPRICE}`)
 }
